dbd1617272b18076a8733b410f4e4d4e8438d62e
Jason Zaitz <jasonzaitz1@gmail.com>
2018-11-27 12:50:32 -0500

    added code challenge

--- CodeChallenge.rb
+++ CodeChallenge.rb
@@ -158,3 +158,21 @@ def valid_parentheses(string)
     end
   end
 end
+
+#You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+def find_outlier(integers)
+  evens = []
+  odds = []
+  integers.each do |x|
+    if x % 2 == 0
+      evens << x
+    elsif x % 2 != 0
+      odds << x
+    end
+  end
+  if evens.length > odds.length
+    return odds[0]
+  elsif odds.length > evens.length
+    return evens[0]
+  end
+end
