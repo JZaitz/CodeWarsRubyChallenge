6cabfec0d015091100d52bd42ed6c988c8f31db0
Jason Zaitz <jasonzaitz1@gmail.com>
2018-11-28 14:49:02 -0500

    added code challenge

--- CodeChallenge.rb
+++ CodeChallenge.rb
@@ -210,3 +210,20 @@ def  first_non_repeating_letter(s)
   end
   return "" if counter == 0
 end
+
+#In this kata, you must create a digital root function.
+
+#A digital root is the recursive sum of all the digits in a number. Given n, take the sum of the digits of n. If that value has two digits, continue reducing in this way until a single-digit number is produced. This is only applicable to the natural numbers.
+def digital_root(n)
+  arr = n.to_s.split("")
+  total = []
+  val, answer = 0, 0
+  arr.each {|x| val += x.to_i}
+  if val <= 9
+    return val
+  elsif val > 9
+    total = val.to_s.split("")
+    total.each {|s| answer += s.to_i }
+    return answer
+  end
+end
